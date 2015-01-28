fb7b4bd4e68f789d2671b7bf9d92c15f3a42e3f9
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-01-28 12:07:19 +0000

    Template for each section

--- js/main.js
+++ js/main.js
@@ -25,33 +25,40 @@ $(function(){
         }).done(function(response){
             // Get the first (main) one
             locale = response.slice(0, 2);
-            // Get the path to the template file
-            fileName = $('#'+ locale).attr('src');
-            // Request to retrieve the relative template file.
-            $.ajax({
-                url: fileName,
-                dataType: 'text'
-            }).done(function(response){
-                //console.log(response);
-                // handle the response and load the compile the template
-                template = Handlebars.compile(response);
-                $('#locale-flag').html('Your locale is: <em>'+ locale.toUpperCase() +'</em>');
-                $('.content').html(template(data));
-                // No template with this name
-            }).fail(function(error){
-                if(error.status === 404){
-                    // If locale is unsupported defaults to english
-                    defaultTemplate = $('#default').html();
-                    // Compile
-                    template = Handlebars.compile(defaultTemplate);
-                    // And insert into the DOM
-                    $('.content').html(template(data));
-                }
-                else{
-                    // All other errors
-                    console.log('Error occurred: ' + error.responseText);
-                }
+
+            $('.localise').each(function(i, k){
+                var _this = $(this),
+                     dataSection = _this.data('section');
+
+                // Get the path to the template file
+                fileName = 'templates/'+ locale +'/'+ dataSection +'.hbs';
+                // Request to retrieve the relative template file.
+                $.ajax({
+                    url: fileName,
+                    dataType: 'text'
+                }).done(function(response){
+                    //console.log(response);
+                    // handle the response and load the compile the template
+                    template = Handlebars.compile(response);
+                    $('#locale-flag').html('Your locale is: <em>'+ locale.toUpperCase() +'</em>');
+                    _this.html(template(data));
+                    // No template with this name
+                }).fail(function(error){
+                    if(error.status === 404){
+                        // If locale is unsupported defaults to english
+                        defaultTemplate = $('#default').html();
+                        // Compile
+                        template = Handlebars.compile(defaultTemplate);
+                        // And insert into the DOM
+                        $('.content').html(template(data));
+                    }
+                    else{
+                        // All other errors
+                        console.log('Error occurred: ' + error.responseText);
+                    }
+                });
             });
+
         });
     });
 

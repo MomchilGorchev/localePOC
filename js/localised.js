42f15a7424fad090cfaff1afcac42d4907fc6d6e
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-01-28 14:04:00 +0000

    OO approach added

---
+++ js/localised.js
@@ -0,0 +1,49 @@
+/**
+ * Created by momchillgorchev on 28/01/15.
+ */
+
+
+
+CONTENT = window.CONTENT || {
+    en: {
+        main_title: 'Localisation POC',
+        sub_header: 'Change your browser locale settings and click the button',
+        main_content: '<p>Each stroke of the hoe was to uncover some aromatic root of wisdom,<br />'+
+                        'heretofore hidden from the sun. Pausing in the field, to let the wind<br />'+
+                        'exhale the moisture from our foreheads, we were to look upward, and catch<br />'+
+                        'glimpses into the far-off soul of truth. In this point of view, matters<br />'+
+                        'did not turn out quite so well as we anticipated.<br />'+
+                      '</p>'+
+                      '<p><strong>Variables passed:</strong></p>'+
+                      '<hr>'+
+                      '<p>name: {{name}}</p>'+
+                      '<p>email: {{email}}</p>'
+    },
+    fr: {
+        main_title: 'Preuve de concept de localisation',
+        sub_header: 'Modifiez les paramètres régionaux de votre navigateur et cliquez sur le bouton',
+        main_content: '<p>'+
+                        'La tribu prophétique aux prunelles ardentes<br />'+
+                        'Hier s`est mise en route, emportant ses petits<br />'+
+                        'Sur son dos, ou livrant à leurs fiers appétits<br />'+
+                        'Le trésor toujours prêt des mamelles pendantes.<br />'+
+                        '<br />'+
+                        'Les hommes vont à pied sous leurs armes luisantes<br />'+
+                        'Le long des chariots où les leurs sont blottis,<br />'+
+                        'Promenant sur le ciel des yeux appesantis<br />'+
+                        'Par le morne regret des chimères absentes.<br />'+
+                        '<br />'+
+                        'Du fond de son réduit sablonneux, le grillon,<br />'+
+                        'Les regardant passer, redouble sa chanson ;<br />'+
+                        'Cybèle, qui les aime, augmente ses verdures,<br />'+
+                            '<br />'+
+                            'Fait couler le rocher et fleurir le désert<br />'+
+                        'Devant ces voyageurs, pour lesquels est ouvert<br />'+
+                        'L`empire familier des ténèbres futures.<br />'+
+                        '</p>'+
+                        '<p><strong>Variables passed:</strong></p>'+
+                        '<hr>'+
+                        '<p>name: {{name}}</p>'+
+                        '<p>email: {{email}}</p>'
+    }
+};
